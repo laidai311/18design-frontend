c9c68fc6b0ae64c25995ce5cbb887728338b9c84
dailai <dai.codetot@gmail.com>
2023-09-08 00:01:48 +0700

    update favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,7 +11,30 @@ export default function MyDocument() {
                     name="googlebot"
                     content="noindex,follow"
                 />
-                <link rel="icon" href="/favicon.ico" />
+                <link
+                    rel="shortcut icon"
+                    href="/favicon.ico"
+                    type="image/x-icon"
+                />
+                <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/apple-touch-icon.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/favicon-32x32.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/favicon-16x16.png"
+                />
+                <link rel="manifest" href="/site.webmanifest" />
             </Head>
             <body>
                 <Main />
